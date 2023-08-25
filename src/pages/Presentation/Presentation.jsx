02b1945c08d741ed3c1c515fd074de7be6517b01
Thibaud91 <59475637+Thibaud91@users.noreply.github.com>
2023-08-25 16:45:13 +0200

    responsive tablet

--- src/pages/Presentation/Presentation.jsx
+++ src/pages/Presentation/Presentation.jsx
@@ -8,15 +8,18 @@ const Presentation = () => {
     <>
       <NavHeader />
       <div className="PresentationDiv">
-        <h1 className="PresentationTitle">L'expertise de Presentation : la construction de stratégies data</h1>
-        <p className="PresentationInfos">
-            Presentation est né de la volonté de proposer une alternative française aux solutions de traitement de données,
-            pour que la data s’exprime avec éthique et intelligence, au service de la population, et dans le respect de la culture française de liberté publique.
-
-            Engagés dans l’innovation au service des territoires, nous fiabilisons et exploitons la richesse de l’open data,
-            nous accompagnons nos clients dans la valorisation de leur patrimoine de données et nous proposons des dispositifs participatifs de création de données.
-
-        </p>
+        <h1 className="PresentationTitle">Pourquoi Spallian ?</h1>
+        <p className="PresentationInfos">Notre entretien téléphonique avec Florian s'est très bien déroulé et il m'a expliqué que Spallian recherchait
+        un alternant fullstack afin de travailler sur Thelma: l'application citoyenne pour la préservation des espaces partagés. Habitant dans le centre de Paris,
+        une application permettant de signaler les problèmes urbains comme les éclairages publics défectueux ou les dépôts sauvages mais également les feux en forêt ou la pollution
+        de rivière dans les espaces naturels m'a rapidement interpellé.</p>
+        <p className="PresentationInfos">Lorsque j'ai commencé mes recherches d'alternance, je me suis fixé trois critères: le projet, l'environnement de travail et les 
+        technologies utilisées par la société. Il m'est arrivé de refuser des offres d'alternance car certains critères étaient trop éloignés de ce que je désirais (par exemple une
+        startup où j'étais le seul développeur à travailler sur une architecture nocode). Travailler chez Spallian serait pour moi l'occasion de poursuivre mon apprentissage
+        du développement dans une entreprise qui a du sens. Je partage vos valeurs d'indépendance, d'audace et d'innovation ainsi que d'éthique et de protection des données.</p>
+        <p className="PresentationInfos">Le fait que votre offre soit fullstack est également important pour moi: je veux apprendre le maximum durant mes un an d'alternance (trois semaines
+        en entreprise et une semaine en formation). Etant en reconversion après une licence de droit, de nombreuses expériences professionelles, puis deux formations fullstack et 
+        de nombreux projets réalisés, j'ai désormais envie de travailler dans le monde du développement et d'apprendre de nouveaux environnements techniques.</p>
       </div>
     </>
   );
